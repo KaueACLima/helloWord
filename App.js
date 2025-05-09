965afceda75cd6dc6f93a32a09bb9a3afdb8313a
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-05-08 23:15:54 -0300

    Atualizar o App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import	{	StatusBar	}	from	'expo-status-bar';
 import	React	from	'react';
+import	React	from	'react';
 import	{	StyleSheet,	Text,	View	}	from	'react-native';
 import	OlaMundo	from	'./componentes/OlaMundo';
 import { Evento } from './componentes/Evento'
